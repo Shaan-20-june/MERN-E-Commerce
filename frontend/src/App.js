bb8e6f88e7cdc8ce829cdb19767b41a9f9ac79ea
Santanu Dutta <santanu.techie32@gmail.com>
2023-05-05 11:14:09 +0530

    Created product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 // import all custom components here
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/products/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
