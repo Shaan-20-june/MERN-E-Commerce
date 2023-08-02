b768fb565ec86cab2758189783e70608b5947393
Shaan-20-june <santanu.techie32@gmail.com>
2023-08-02 10:37:47 +0530

    Added product routes and custom async error handler

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+
+// import all routers
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -13,15 +15,7 @@ app.get('/', (req, res) => {
   res.send('API is working!')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const productId = req.params.id
-  const product = products.find((product) => product._id === productId)
-  res.json(product)
-})
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000
 
