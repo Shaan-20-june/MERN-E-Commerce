7b28ed4afaf77146fe0bd686cf9228a82bf48cb3
Shaan-20-june <santanu.techie32@gmail.com>
2023-08-03 10:26:07 +0530

    Created custom error handler middleware and product controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,25 +1,13 @@
 import express from 'express'
-import asyncHandler from '../middlewares/asyncHandler.js'
-import Product from '../models/productModel.js'
+import {
+  getProductById,
+  getProducts,
+} from '../controllers/productController.js'
 
 const router = express.Router()
 
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-  })
-)
+router.route('/').get(getProducts)
 
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const productId = req.params.id
-    const product = await Product.findById(productId)
-    if (product) return res.json(product)
-    res.status(404).json({ message: 'Product not found' })
-  })
-)
+router.route('/:id').get(getProductById)
 
 export default router
