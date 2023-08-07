4bba53110b85da18682614d5b40c62d700ba65e8
Shaan-20-june <santanu.techie32@gmail.com>
2023-08-08 00:58:05 +0530

    Added user routes and authentications

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 
 // import all routers
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -12,12 +14,22 @@ connectDB()
 
 const app = express()
 
+// Body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Cookie parser middleware
+app.use(cookieParser())
+
 app.get('/', (req, res) => {
   res.send('API is working!')
 })
 
+// Use the routes
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
+// Error handler middlewares
 app.use(notFound)
 app.use(errorHandler)
 
